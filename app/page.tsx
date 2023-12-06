2d0c3d933125c611db25d107cae6682ddcea7551
JPHartman <33009892+JPHartman@users.noreply.github.com>
2023-12-05 22:24:18 -0700

    Added Settings and Shortcuts (#1)
    * Added Settings and Shortcuts
    
    added shortcuts for links on page and a settings tab to edit them
    
    * Update page.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,38 @@
-"use client"
-import {useState, useEffect} from "react"
-import Background from "./background"
-import settings from "./settings"
+'use client'
+import {useState, useEffect} from 'react'
+import Background from './background'
+import Settings from './settings'
+import './page.css'
 
+interface Website {
+  name: string;
+  url: string;
+  selected: boolean;
+}
+const initialWebsites: Website[] = [
+    { name: 'D2L', url: 'https://ecat.montana.edu/d2l/home', selected: true },
+    { name: 'Youtube', url: 'https://youtube.com', selected: true }
+  ];
 export default function Page() {
+  const [shortcutWebsites, setShortcutWebsites] = useState<Website[]>(initialWebsites);
+  const updateShortcuts = (websites: Website[]) => {
+    setShortcutWebsites(websites.filter((website) => website.selected))
+  };
 
-    return (
+  return (
     <div>
-        <Background state={0} />
+      <Background state={0} />
+      <div className ="settings-icon-container">
+      <Settings updateShortcuts={updateShortcuts} />
+      </div>
+      <div className="shortcuts-container"> 
+        {shortcutWebsites.map((website, index) => (
+          <a key={index} href={website.url} target="_blank" rel="noopener noreferrer">
+            {website.name}
+          </a>
+        ))}
+      </div>
+      
     </div>
-    )
+  );
 }
--- app/settings.tsx
+++ app/settings.tsx
@@ -1,10 +1,64 @@
+// settings.tsx
+import { useState } from 'react';
+import Image from 'next/image';
+import settingsIcon from './images/settings.png';
 
+interface SettingsProps {
+  updateShortcuts: (websites: Website[]) => void;
+}
 
-export default function settings() {
-    
-    return (
+interface Website {
+  name: string;
+  url: string;
+  selected: boolean;
+}
+
+export default function Settings(props: SettingsProps) {
+  const [isCollapsed, setIsCollapsed] = useState(true); // State to track whether the settings panel is collapsed
+  const [websites, setWebsites] = useState<Website[]>([
+    { name: 'Youtube', url: 'https://youtube.com', selected: false },
+    { name: 'Instagram', url: 'https://instagram.com', selected: false },
+    { name: 'D2L', url: 'https://ecat.montana.edu/d2l/home', selected: false },
+    { name: 'Facebook', url: 'https://www.facebook.com/', selected: false },
+    { name: 'Netflix', url: 'https://www.netflix.com/', selected: false },
+  ]);
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  const toggleWebsite = (index: number) => {
+    const updatedWebsites = [...websites];
+    updatedWebsites[index].selected = !updatedWebsites[index].selected;
+    setWebsites(updatedWebsites);
+    props.updateShortcuts(updatedWebsites); // Update shortcuts after changing selection
+  };
+
+  return (
+    <div>
+      <div onClick={toggleCollapse} className="settings-icon">
+        <Image src={settingsIcon.src} alt="Settings Icon" width={50} height={50} />
+      </div>
+
+      {/* Display websites and checkboxes only if the panel is not collapsed */}
+      {!isCollapsed && (
         <div>
-            
+          {websites.map((website, index) => (
+            <div key={index}>
+              <label>
+                <a href={website.url} target="_blank" rel="noopener noreferrer">
+                  {website.name}
+                </a>
+                <input
+                  type="checkbox"
+                  checked={website.selected}
+                  onChange={() => toggleWebsite(index)}
+                />
+              </label>
+            </div>
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
